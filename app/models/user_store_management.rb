c8ea2415b693d71c1dca60bed804e88c2fac651c
Ankur <ankur.jain@ongraph.com>
2014-06-05 14:30:11 +0530

    change store to organization

--- app/models/user_store_management.rb
+++
@@ -1,6 +0,0 @@
-class UserStoreManagement < ActiveRecord::Base
-	store_accessor :contact, :order, :invoice, :commission, :user_rep
-
-	belongs_to :user
-	belongs_to :store
-end
--- db/migrate/20140604063337_add_contact_to_user_store_management.rb
+++
@@ -1,9 +0,0 @@
-class AddContactToUserStoreManagement < ActiveRecord::Migration
-  def change
-    add_column :user_store_managements, :contact, :hstore
-    add_column :user_store_managements, :invoice, :hstore
-    add_column :user_store_managements, :orders, :hstore
-    add_column :user_store_managements, :commission, :hstore
-    add_column :user_store_managements, :user_rep, :hstore
-  end
-end
